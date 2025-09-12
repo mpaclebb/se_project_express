bf10d3f51a8302f38279a7302c3b24ffe6151b74
Marku Pacleb <markupacleb@gmail.COM>
2025-09-12 19:55:55 -0400

    middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,2 +1,27 @@
 const jwt = require("jsonwebtoken");
-const { UNAUTHORIZED_STATUS} = 
+const { UNAUTHORIZED_STATUS } = require("../utild/erros");
+const { JWT_SECRET} = require("../utils/config");
+
+const auth = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res
+      .status(UNAUTHORIZED_STATUS)
+      .send({ message: "Authorization required" });
+  }
+
+  const token = authorization.replace("Bearer ", "");
+
+  let payload;
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    return res.status(UNAUTHORIZED_STATUS).send({ message: "Invalid token" });
+  }
+
+  req.user = payload;
+  next();
+};
+
+module.exports = auth;
